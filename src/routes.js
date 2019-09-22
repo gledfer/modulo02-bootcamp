362d3d1ac529cfececbb746e96a8288a559d6d08
gledfer <gledson.fernandes.santos@gmail.com>
2019-09-22 20:47:53 -0300

    chore: create pagination

--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,8 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/appointments', AppointmentController);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
